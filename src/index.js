7d9699ad818c3b35b748fc6ea94ff5b7b2056f5f
Iultin <iultin@mail.ru>
2022-01-05 18:59:34 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let number_sign = 1;
+  if (n < 0) {
+    number_sign = -1;
+    n *= -1;
+  }
+  let string_n = n.toString();
+  let result = '';
+  for (let i = string_n.length - 1; i >= 0; i--) {
+    result += string_n[i];
+  }
+  return +result
 }
